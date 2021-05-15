f39d5936a5c34d183c127d9377dbd982893844de
Martin-Carlsson85 <martin@webfixit.se>
2021-05-15 16:24:18 +0200

    Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -57,4 +57,66 @@ $('a[href*="#"]')
     }
   }
 });
+
+    /* Animations on scroll */
+    
+    $('.js--wp-1').waypoint(  
+        (direction) => {
+        // add the class animated__animated and animate__fadeIn to waypoint 1
+          $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+        },
+        {
+          offset: '50%',
+        }
+      );
+    //When you reach waypoint 2
+      $('.js--wp-2').waypoint(
+        (direction) => {
+    // add the class animated__animated and animate__fadeInLeft to waypoint 2
+          $('.js--wp-2').addClass('animate__animated animate__fadeInLeft');
+        },
+        {
+          offset: '50%',
+        }
+      );
+    //When you reach waypoint 3
+      $('.js--wp-3').waypoint(
+        (direction) => {
+     // add the class animated__animated and animate__fadeIn to waypoint 3
+          $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+        },
+        {
+          offset: '50%',
+        }
+      );
+    //When you reach waypoint 4
+      $('.js--wp-4').waypoint(
+        (direction) => {
+    // add the class animated__animated and animate__bounceIn to waypoint 4
+          $('.js--wp-4').addClass('animate__animated animate__bounceIn');
+        },
+        {
+          offset: '25%',
+        }
+      );
+
+
+
+    $('.js--header').waypoint(
+        (direction) => {
+        if (direction === 'up') {
+    // remove the animation classes from all waypoints (1 - 4)
+            $('.js--wp-1').removeClass('animate__animated animate__fadeIn');
+            $('.js--wp-2').removeClass('animate__animated animate__fadeInLeft');
+            $('.js--wp-3').removeClass('animate__animated animate__fadeIn');
+            $('.js--wp-4').removeClass('animate__animated animate__bounceIn');
+        }
+        },
+        {
+        offset: '-25%',
+        }
+    );
+
+
+
 });
