a2b1a572bbd7dea5cc3da41ea1282e9c9a9713e5
Martin-Carlsson85 <martin@webfixit.se>
2021-05-16 14:36:03 +0200

    Making the navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -117,6 +117,24 @@ $('a[href*="#"]')
         }
     );
 
+    /*Mobile nav*/
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon');
 
+        nav.slideToggle(200);
+    
+    });
+    
+        // if(icon.hasName('menu-outline')) {
+        //     icon.addName('close-outline')
+        //     icon.removeName('menu-outline');
+        // } else {
+        //     icon.addName('menu-outline');
+        //     icon.removeName('close-outline');
+        // }
+    
 
+    // <ion-icon name="close-outline"></ion-icon>
+    // ion-close-round
 });
